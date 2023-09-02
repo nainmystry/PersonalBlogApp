f58f7da041961bd657595a1591878cdb9ab1a9f1
Ramesh Mistry <nayan.ramesh.mistry@accenture.com>
2023-09-02 13:48:59 +0530

    Initial Project add

---
+++ PersonalBlogUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PersonalBlogUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
